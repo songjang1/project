64ad08b5e97d0c3e2f5a6d13601099519823e3c2
songjang1 <tbvj0111@gmail.com>
2017-06-14 15:43:13 +0900

    Add files via upload

---
+++ ArgsTest01.java
@@ -0,0 +1,11 @@
+//package chap05;
+
+public class ArgsTest01 {
+
+	public static void main(String[] args) {
+		System.out.println("명령행 매개변수===>"+args[0]);
+		System.out.println("명령행 매개변수===>"+args[1]);
+		System.out.println(args[0]+args[1]);
+	}
+
+}
